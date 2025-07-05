341d5b4e4417bf43c95b888973db8a2337a24055
타래 <85001878+meowTarae@users.noreply.github.com>
2025-07-05 20:31:39 +0900

    [✨feat]: 카운터 기능 구현

--- dev-env/react/src/App.tsx
+++ dev-env/react/src/App.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    <section className="flex flex-col items-center justify-center h-screen">
+      <span className="mb-8">{number}</span>
+      <div className="flex gap-2">
+        <button
+          aria-label="increase button"
+          className="w-10 h-10 border-2 border-white cursor-pointer select-none"
+          onClick={() => setNumber((prev) => prev + 1)}
+        >
+          +
+        </button>
+        <button
+          aria-label="decrease button"
+          className="w-10 h-10 border-2 border-white cursor-pointer select-none"
+          onClick={() => setNumber((prev) => prev - 1)}
+        >
+          -
+        </button>
+      </div>
+    </section>
   );
 }
 
